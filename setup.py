e861280fd2c9e0ccca0c4a538e8be5533840da34
drkatnz <sonic@sonic.net.nz>
2016-10-24 23:23:29 +1300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='CombinedOneClass',
+    version='0.1dev',
+    packages=['oneclass'],
+    license='MIT License',
+    long_description=open('README.md').read(),
+)
